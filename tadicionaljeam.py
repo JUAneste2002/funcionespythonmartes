e0d3537fb31c810f2d19762cc9faf9b48693de1f
azxsax <128758666+cmilitogces2023@users.noreply.github.com>
2024-04-09 19:23:33 -0500

    nt8 se crea ejemplo de funcion tradicional

---
+++ tadicionaljeam.py
@@ -0,0 +1,19 @@
+numeroUno=5
+numeroDOS=10 
+sumatoria=numeroUno+numeroDOS 
+print(f"la suma es{sumatria}") 
+
+numeroUno=5
+numeroDOS=10 
+sumatoria=numeroTres+numeroCuatro 
+print(f"la suma es{sumatriaDos}")
+
+
+def sumarNumeros(numeroUno,numeroDOS):
+    sumatoria=numeroUno+numeroDOS 
+    return sumatoria
+numeroUno=5
+numeroDOS=20 
+numeroTres=50
+resultado=sumarNumeros(10,20) 
+print(f"la suma es:{resultado}")
