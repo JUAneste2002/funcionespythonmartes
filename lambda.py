d8d593df2b1f196ba8b2f1268d3d68b8d725d0e0
azxsax <128758666+cmilitogces2023@users.noreply.github.com>
2024-04-09 20:29:18 -0500

    nt9 feat se crea ejemplo con funcion lambada

---
+++ lambda.py
@@ -0,0 +1,8 @@
+#declarando una funcion lambda
+sumarNumeros=lambda numero1, numero2:numero1+numero2 
+
+#llamado una funcion lambda 
+resultado=sumarNumeros(10,10)
+print(f"la suma es{resultado}")
+
+
